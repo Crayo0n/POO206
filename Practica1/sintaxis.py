4fbbc9c1eda8cb3272ff3838a58f8352be87b86e
Crayo0n <mauricio28rd@gmail.com>
2025-05-16 18:19:00 -0600

    Variables en Python

--- Practica1/sintaxis.py
+++ Practica1/sintaxis.py
@@ -11,3 +11,25 @@ en python
 #2.- Strings 
 print("Hola soy una cadena") 
 print('Yo soy otra')
+variable1="hola soy una cadena"
+print(len(variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3.- Variables
+
+x="Mauricio Rodriguez"
+x= 4
+x=5.76
+
+print(x)
+
+x= int(3)
+y= float(3)
+z=str(3)
+
+print (x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
