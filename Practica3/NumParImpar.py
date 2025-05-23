0143a30acb348f7a1ba8e0c8765338690618c365
Crayo0n <mauricio28rd@gmail.com>
2025-05-22 20:06:18 -0600

    Corrección de números Par e Impar

--- Practica3/NumParImpar.py
+++ Practica3/NumParImpar.py
@@ -1,11 +1,16 @@
 while True:
+    
     try: 
         num= int(input("Introduce un número: "))
-        if (num%2==0):
-            print("Tu numero ingresado es par")
+        if num>0:
+            if (num%2==0):
+                print("Tu numero ingresado es par")
+            else:
+                print("Tu numero ingresado es impar ")
         else:
-            print("Tu numero ingresado es impar ")
-    except (ValueError,): 
+            print("No se puede ingresar números negativos o 0")
+    except (ValueError): 
         print("Error: Se ingreso algo diferente a un número")
+        
         break
 
