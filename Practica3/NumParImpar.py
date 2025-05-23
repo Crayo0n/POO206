92f04c30989a07fe44b956e4b8b55ab6296449e2
Mauricio <mauricio28rd@gmail.com>
2025-05-22 22:17:47 -0600

    Correción 2.0 Par o Impar

--- Practica3/NumParImpar.py
+++ Practica3/NumParImpar.py
@@ -1,16 +1,21 @@
 while True:
     
     try: 
+        print("Introduce 100 para finalizar\n")
         num= int(input("Introduce un número: "))
-        if num>0:
-            if (num%2==0):
-                print("Tu numero ingresado es par")
-            else:
-                print("Tu numero ingresado es impar ")
+        if num == 100:
+            print("\nFin del programa")
+            break
         else:
-            print("No se puede ingresar números negativos o 0")
+            if num>0:
+                if (num%2==0):
+                    print("Tu numero ingresado es par\n")
+                else:
+                    print("Tu numero ingresado es impar \n")
+            else:
+                print("No se puede ingresar números negativos o 0")
     except (ValueError): 
         print("Error: Se ingreso algo diferente a un número")
         
-        break
+       
 
